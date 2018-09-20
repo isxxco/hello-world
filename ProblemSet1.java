ad72a8dc71e30a70389949ba7dce98e4af7c0743
isxxco <43239925+isxxco@users.noreply.github.com>
2018-09-20 15:28:53 -0400

    Update ProblemSet1.java

--- ProblemSet1.java
+++ ProblemSet1.java
@@ -1,5 +1,46 @@
-public class Hello {
+/**
+ * INSTRUCTIONS.
+ * 
+ * Problem Set 1 will test your skills of printing to the console. You'll
+ * be asked to print various text, shapes, and figures, all of which is
+ * explained (and demonstrated) in detail in the README file.
+ */
+
+public class ProblemSet1 {	
+	
+	/**
+	 * Every Java program must have a main method, which is
+	 * where program execution begins.
+	 */
+	
 	public static void main(String[] args) {
-		System.out.println("Hello, AP Computer Science!");
+	System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+      	System.out.println("   ##    ######  ######## ########");
+      	System.out.println("  #  #   #     # #        #");
+      	System.out.println(" #    #  #     # #        #");
+      	System.out.println("######## ######  #        ########");
+      	System.out.println("#      # #       #               #");
+        System.out.println("#      # #       #               #");
+      	System.out.println("#      # #       ######## ########");
+		
+	System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+      	System.out.println("  /\\     /\\     /\\");
+      	System.out.println(" /  \\   /  \\   /  \\");
+      	System.out.println("/    \\ /    \\ /    \\");
+	System.out.println("\\    / \\    / \\    /");
+      	System.out.println(" \\  /   \\  /   \\  /");
+      	System.out.println("  \\/     \\/     \\/");
+      
+	System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+      	System.out.println("   \\\\\\\\\\");
+      	System.out.println("  +\"\"\"\"\"+");
+      	System.out.println("(|  0 0  |)");
+      	System.out.println(" |   ^   |");
+      	System.out.println(" |  \'-\'  |");
+      	System.out.println("  +-----+");
+		
+		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
+		
+		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
 	}
 }
